9575bd7c13c78fc9d2327e238d5c61902c0aa8bb
dartmew <dartmew@yandex.com>
2026-08-08 21:17:02 +0300

    Добавил модель pokemon, создал миграции, применил миграции, зарегистрировал модель в админке

---
+++ pokemon_entities/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Pokemon
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,11 @@
 from django.db import models  # noqa F401
 
 # your models here
+class Pokemon(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='pokemon_name'
+    )
+
+    def _str_(self):
+        return self.title
